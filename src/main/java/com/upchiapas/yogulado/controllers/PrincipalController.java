2a6ad129c0b330e961977d4b0ba40040fba208fd
Nancy1226 <Jien260819>
2022-07-08 20:33:44 -0500

    primer commit

---
+++ src/main/java/com/upchiapas/yogulado/controllers/PrincipalController.java
@@ -0,0 +1,14 @@
+package com.upchiapas.yogulado.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PrincipalController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.upchiapas.yogulado {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.upchiapas.yogulado to javafx.fxml;
+    exports com.upchiapas.yogulado;
+    exports com.upchiapas.yogulado.controllers;
+    opens com.upchiapas.yogulado.controllers to javafx.fxml;
+}
