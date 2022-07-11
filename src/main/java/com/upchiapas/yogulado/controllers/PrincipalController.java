7e33007a2030bb14398e7e02e1b6461d741eb4b2
Nancy1226 <nancy-12_03@hotmail.com>
2022-07-10 23:59:58 -0500

    actualizacion 2

--- src/main/java/com/upchiapas/yogulado/controllers/PrincipalController.java
+++ src/main/java/com/upchiapas/yogulado/controllers/PrincipalController.java
@@ -1,57 +1,42 @@
 package com.upchiapas.yogulado.controllers;
 
-import com.upchiapas.yogulado.HelloApplication;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class PrincipalController {
 
     @FXML
-    private Label lblContacto;
+    private Label bInicio;
 
     @FXML
-    private Label lblInicio;
+    private Label blContacto;
 
     @FXML
-    private Label lblMenu;
+    private Label blMenu;
 
     @FXML
     private Label lblSalir;
 
     @FXML
-    void lblInicioMouse(MouseEvent event) {
+    void lbMenuMouseClicked(MouseEvent event) {
 
     }
 
     @FXML
-    void lblMenuMouseClicked(MouseEvent event) {
-        FXMLLoader loader=new FXMLLoader(HelloApplication.class.getResource("Menu.fxml"));
-        MenuController controladores=loader.getController();
-        Parent root=null;
-        try {
-                root=loader.load();
-        }catch (IOException e){
-            throw new RuntimeException(e);
-        }
-        Scene scene=new Scene(root);
-        Stage stage=new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setScene(scene);
-        stage.show();
+    void lblContactoMouseClicked(MouseEvent event) {
+
     }
 
     @FXML
-    void lblSalirMouseCliked(MouseEvent event) {
+    void lblInicioMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void lblSalirMouseClicked(MouseEvent event) {
         System.exit(1);
     }
 
 }
+
